5cad81c8f8796257649f25ef0f558943e47519ec
HsiaoHungWang <wang@ispan.com.tw>
2023-11-04 14:54:08 +0800

    html-webpack-plugin介紹

--- WebPackDemo/src/main.js
+++ WebPackDemo/src/main.js
@@ -1,4 +1,4 @@
 import {person} from './person';
 
 const myDiv = document.querySelector("#app");
-myDiv.innerHTML = `<h2>${person.firstName} ${person.lastName}</h2>`
+myDiv.innerHTML = `<h2 style="color:green">${person.firstName} ${person.lastName}</h2>`
--- WebPackDemo/webpack.config.js
+++ WebPackDemo/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
  entry:'./src/main.js',
  output:{
     filename:'bundle.js',
     path:path.resolve(__dirname, 'dist')
- }
+ },
+ plugins:[new HtmlWebpackPlugin({
+    title:'webpack dmeo',
+    //filename:'abc.html'
+    template:'./template.html'
+ })]
 
 }
