acb475aacdfe511309f98d3bf43496b80399a1ee
HsiaoHungWang <wang@ispan.com.tw>
2023-11-05 12:27:51 +0800

    建立Component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first.component';
+import { SecondComponent } from './second/second.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { XyzComponent } from './xyz.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/xyz.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abc',
+  template: `
+    <p>
+      xyz works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class XyzComponent {
+
+}
