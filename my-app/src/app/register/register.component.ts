935087ce9477d549bc82fec212ead077339f19fb
HsiaoHungWang <wang@ispan.com.tw>
2023-11-12 14:16:26 +0800

    FormBuilder 的使用

--- my-app/src/app/register/register.component.ts
+++ my-app/src/app/register/register.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-      userName : new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
-      userEmail : new FormControl('',[Validators.required, Validators.email]),
-      userPhone : new FormControl('',[Validators.required, Validators.pattern(/^09\d{2}-\d{3}-\d{3}$/)])
-  });
+
+  constructor(private fb:FormBuilder){}
+  //FormGroup的使用
+  // registerForm = new FormGroup({
+  //     userName : new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+  //     userEmail : new FormControl('',[Validators.required, Validators.email]),
+  //     userPhone : new FormControl('',[Validators.required, Validators.pattern(/^09\d{2}-\d{3}-\d{3}$/)])
+  // });
+
+  //FormBuilder的使用
+  registerForm = this.fb.group({
+    userName : ['',[Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+    userEmail : ['',[Validators.required, Validators.email]],
+    userPhone : ['',[Validators.required, Validators.pattern(/^09\d{2}-\d{3}-\d{3}$/)]]
+});
 
   get f(){
     return this.registerForm.controls;
